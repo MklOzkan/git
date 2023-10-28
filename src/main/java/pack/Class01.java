6460df9324b576ba4ae7f54818aa4594b2d8579b
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2023-10-28 20:31:10 +0200

    v2

--- src/main/java/pack/Class01.java
+++ src/main/java/pack/Class01.java
@@ -1,7 +1,14 @@
 package pack;
 
+import java.util.Scanner;
+
 public class Class01 {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String s = scan.nextLine();
+        System.out.println("your name: " + s);
     }
 }
